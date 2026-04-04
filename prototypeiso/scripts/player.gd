1f72aeaeafad41d4ad9b92f2558424c5432deeea
Mr Coconut <bradley@sellers-blais.com>
2026-04-04 08:28:30 -0700

    Fixed some movement bugs :)

--- prototypeiso/scripts/player.gd
+++ prototypeiso/scripts/player.gd
@@ -2,32 +2,28 @@
 extends CharacterBody2D
 
 #speed
-const SPEED = 300.0
+const SPEED = 100.0
 
 #movement
-func _process(_delta):
-	if Input.is_action_pressed("right"):
-		velocity.x += 1
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
+func movement():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * SPEED
 
 #calling all functions!
 func _physics_process(_delta: float) -> void:
+	movement()
 	animation()
+	move_and_slide()
 	
 #handling animation
 func animation():
 	if velocity.y == 0 and velocity.y == 0:
 		$AnimatedSprite2D.play("down")
-	if velocity.y == 1:
+	if Input.is_action_pressed("down"):
 		$AnimatedSprite2D.play("down")
-	if velocity.y == -1:
+	if Input.is_action_pressed("up"):
 		$AnimatedSprite2D.play("up")
-	if velocity.x == 1:
+	if Input.is_action_pressed("right"):
 		$AnimatedSprite2D.play("right")
-	if velocity.x == -1:
-		$AnimatedSprit2D.play("left")
+	if Input.is_action_pressed("left"):
+		$AnimatedSprite2D.play("left")
